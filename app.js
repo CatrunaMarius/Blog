2d44c66822455540ad129a1da496a0a5ba243fad
Marius_haru <roberto_marius12@yahoo.com>
2020-08-19 21:15:48 +0300

    upgrade blog with mongoDB

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // challenge 18
 const _ = require('lodash');
+// challenge upgrade blog
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,7 +14,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const Posts = [];
+
 
 app.set('view engine', 'ejs');
 
@@ -20,14 +22,68 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/", function(req,res){
-  // challenge 12
+// challenge upgrade blog
+mongoose.connect('mongodb://localhost/blogDB', {useNewUrlParser: true});
+const postSchema ={
+  title: String,
+  content: String
+}
 
- 
-  res.render("home", {text:homeStartingContent, post:Posts} );
+const Post =mongoose.model("Post",postSchema )
+
+
+app.get("/", function(req,res){
+ // challenge upgrade blog
+
+  Post.find({}, function(err,posts){
+    res.render("home", {text:homeStartingContent, post:posts} );
+  })
+  
   
    
 })
+
+
+
+app.get("/compose", function(req,res){
+  res.render("compose")
+})
+
+// challenge upgrade blog
+app.post("/compose", function(req, res){
+   
+    const post = new Post( {title: req.body.postTitle,
+                content: req.body.postBody})
+    
+    post.save(function(err){
+      if(!err){
+        res.redirect("/");
+      }
+    })
+   
+    
+})
+
+// challenge upgrade blog
+
+app.get("/posts/:postId",function(req, res){
+  
+  const requestedPostId =req.params.postId
+  
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+ 
+
+
+
+
+  
+});
+
 
 app.get("/about", function(req, res){
   res.render("about.ejs", {about:aboutContent})
@@ -38,43 +94,8 @@ app.get("/contact", function(req, res){
 
 })
 
-app.get("/compose", function(req,res){
-  res.render("compose")
-})
-
-// challenge 11
-app.post("/compose", function(req, res){
-    //crearea unui obiect
-    const post = {title  : req.body.postTitle,
-                content: req.body.postBody}
-    
-    Posts.push(post)
-    res.redirect("/");
-    
-})
-
-// challenge 19 (odata ce esti intr-o bucla for nu poti folosi const Posts ci post)
-app.get("/posts/:post",function(req, res){
-  
-  const link =_.lowerCase(req.params.post)
-  
-  Posts.forEach(function(post){
-    const title = _.lowerCase(post.title); 
-   
-
-    if (title === link){
-      console.log("match found");
-      res.render("post", {title:post.title ,content:post.content})
-      
-   
-    }
-  });
 
 
-
-
-  
-});
 
 
 app.listen(9000, function() {
